8030dff3a2d457c4e223b34f47b2199028064130
Travis Wagner <travis.wagner@umusic.com>
2024-06-26 22:13:25 -0400

    moved shared stuff to ./shared.ts

--- src/OrderedMap.ts
+++ src/OrderedMap.ts
@@ -1,36 +1,4 @@
-export enum Ordering {
-    Less = -1,
-    Equal = 0,
-    Greater = 1,
-}
-
-export interface Eq<T> {
-    eq(other: T): boolean;
-}
-
-export function implementsEq<T>(x: T | Eq<T>): x is Eq<T> {
-    return (
-        typeof x === "object" &&
-        x !== null &&
-        "eq" in x &&
-        typeof x.eq === "function" &&
-        x.eq.length === 1
-    );
-}
-
-export interface Cmp<T> {
-    cmp(other: T): Ordering;
-}
-
-export function implementsCmp<T>(x: T | Cmp<T>): x is Cmp<T> {
-    return (
-        typeof x === "object" &&
-        x !== null &&
-        "cmp" in x &&
-        typeof x.cmp === "function" &&
-        x.cmp.length === 1
-    );
-}
+import { defaultEqualityFn, defaultComparatorFn } from "./shared";
 
 export class OrderedMap<K, V> extends Map<K, V> {
     private equalityFn: (a: K, b: K) => boolean;
@@ -128,20 +96,4 @@ export class OrderedMap<K, V> extends Map<K, V> {
     [Symbol.iterator](): IterableIterator<[K, V]> {
         return this.entries();
     }
-}
-
-function defaultEqualityFn<K>(a: K, b: K): boolean {
-    if (implementsEq(a)) {
-        return a.eq(b);
-    }
-    return a === b;
-}
-
-function defaultComparatorFn<K>(a: K, b: K): Ordering {
-    if (implementsCmp(a)) {
-        return a.cmp(b);
-    }
-    if (a < b) return Ordering.Less;
-    if (a > b) return Ordering.Greater;
-    return Ordering.Equal;
 }
